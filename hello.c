74419f4346beb43c9ab33b2346110a005bd6c472
kiran <kiranghorpade0014@gmail.com>
2022-10-05 14:13:21 +0000

    first program

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
+
+    printf("first hello world program for git");
     printf("hello world.");
     printf("this is second line");
     printf("merge resolve");
