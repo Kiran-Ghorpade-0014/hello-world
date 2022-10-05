78770d15f19c4b8e4d6b68452605d4bbfb3b1ce9
kiran <kiranghorpade0014@gmail.com>
2022-10-05 13:18:24 +0000

    emergency fix for hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include<stdio.h>   
 
 int main(int argc, char const *argv[])
 {
     printf("hello world.");
     printf("this is second line");
+    printf("line for emergency fix");
     return 0;
 }
