85e47a6bd12db334c19d54a11a7333a08d3423f9
kiran <kiranghorpade0014@gmail.com>
2022-10-05 13:22:48 +0000

    added new line hello.c

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[])
 {
     printf("hello world.");
     printf("this is second line");
-    printf("this is third line.")
+    printf("this is third line.");
+    printf("merge conflict");
     return 0;
 }
